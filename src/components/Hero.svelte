b7ab830870cc47fcf867febb83439b299c6a2ff9
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-09-28 20:14:38 +0300

    fix: copy button on hero

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -4,7 +4,13 @@
     <div>
       <h1 class="mb-5 text-4xl font-black lg:text-5xl xl:text-6xl">{@html title}</h1>
       <p class="mb-5 text-4xl font-black -rotate-6 font-handwriting text-secondary lg:text-5xl xl:text-6xl">{@html desc}</p> 
-      <slot/>
+      <div class="max-w-md mx-auto">
+        <Codes 
+          client:load
+          textToCopy="https://api.lorem.space/image?w=400&h=400"
+          showListOfLinks=false
+        >https://api.lorem.space/image?w=<span class="text-primary">400</span>&h=<span class="text-primary">400</span></Codes>
+      </div>
     </div>
   </div>
 
@@ -12,6 +18,7 @@
 
 
 <script>
+  import Codes from '../components/Codes.svelte'
   export let title = "";
   export let desc = "";
 </script>
